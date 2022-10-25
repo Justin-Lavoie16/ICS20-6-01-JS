680709daaaa0206ad229bdacd93490ba227c0c8f
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-25 18:49:40 +0000

    324

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+;("use strict")
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/Justin-Lavoie16/Unit-6-01-JS", {
+    scope: "/Justin-Lavoie16/",
+  })
+}
+
+function calculate() {
+  const length = parseInt(document.getElementById("pay").value)
+  const width = parseInt(document.getElementById("pay2").value)
+
+  const area = length * width * 0.18
+  const perimeter = length * width * (1.0 - 0.18)
+  const TAX_RATE = 0.18
+
+  document.getElementById("area").innerHTML =
+    "The governemnt will take: " + area + "$"
+  document.getElementById("perimeter").innerHTML =
+    "your pay will be: " + perimeter + "$"
+}
